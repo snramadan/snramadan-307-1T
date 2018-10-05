4162cf985f98438d00e539dcbf80d678e53bb710
Saba Ramadan <43482757+snramadan@users.noreply.github.com>
2018-10-05 10:53:19 -0700

    TERM 1

---
+++ src/MultiVars.java
@@ -0,0 +1,11 @@
+
+public class MultiVars {
+	public static void multiVar(String[] args)
+	{
+		int i = 10;
+	    int j = 2;
+	    int k;
+	    k = i * j;
+	    System.out.println("i * j is " + k);
+	}
+}
